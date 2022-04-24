1815aa6de5492b19f5cde66494cc38867211b643
Kimberly <kimolanga123@gmail.com>
2022-04-24 14:15:50 +0300

    add saving multiple passwords option

--- password.py
+++ password.py
@@ -5,31 +5,41 @@ class User:
 
     passwords_list = [] #List is empty..
     def __init__(self,first_name,last_name,email,passwords):
-            # """
-            # __init__ methods that helps us define properties for our objects.
+        # """
+        # __init__ methods that helps us define properties for our objects.
 
-            # Args:
-            #     first_name:New password first name.
-            #     last_name:New password last name.
-            #     email:New password email address.
-            #     password:New password password.
-            # """
-
-            self.first_name = first_name
-            self.last_name = last_name
-            self.email = email
-            self.passwords = passwords
+        # Args:
+        #     first_name:New password first name.
+        #     last_name:New password last name.
+        #     email:New password email address.
+        #     password:New password password.
+        # """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.passwords = passwords
 
     def save_passwords(self):
         """
         save_passwords method saves passwords objects into passwords_list
         """
         User.passwords_list.append(self)
-
+    
     def tearDown(self):
         """
         tearDown method that does clean up after each test case has run
         """
         User.passwords_list = []
 
-    def
+    def test_save_multiple_passwords(self):
+        """
+        test_save_multiple_passwords to check if we can save multiple passwords objects to our passwords_list
+        """
+        self.new_passwords.save_passwords()
+        test_passwords = User("Cheryl","Hadassah","chadassah@gmail.com","Chada12")
+        test_passwords.save_passwords()
+        self.assertEqual(len(User.passwords_list),2)
+
+
+if __name__ =='__main__':
+    unittest.main()    
--- password_test.py
+++ password_test.py
@@ -2,37 +2,35 @@ import unittest
 from password import User
 
 class TestCredentials(unittest.TestCase):
-    """
-    Test class that defines test cases for the User class behaviors.
-
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
-    """
-
-    def setUp(self):
+ """
+ Test class that defines test cases for the User class behaviors.
+ Args:
+    unittest.TestCase: TestCase class that helps in creating test cases
+ """
+        
+ def setUp(self):
         """
         set up method to run before each test cases.
         """
         self.new_passwords = User("Kimzy","Eve","kimzyeve@gmail.com","Ayubu12")
 
-    def test_init(self):
+ def test_init(self):
         """
         test_init test case to test if the object is initialized properly
         """
-
         self.assertEqual(self.new_passwords.first_name,"Kimzy")
         self.assertEqual(self.new_passwords.last_name,"Eve")
         self.assertEqual(self.new_passwords.email,"kimzyeve@gmail.com")
         self.assertEqual(self.new_passwords.passwords,"Ayubu12")
 
-    def test_save_passwords(self):
+ def test_save_passwords(self):
         """
-        test_save_passwords test case to test if the contact object is saved tnto the passwords list
+        test_save_passwords test case to test if the contact object is saved into the passwords_list
         """
         self.new_passwords.save_passwords()
-        self.assertEqual(len(User.passwords_list),1)
+        self.assertEqual(len(User.passwords_list),3)
 
-    def test_save_multiple_passwords(self):
+ def test_save_multiple_passwords(self):
         """
         test_save_multiple_passwords to check if we can save multiple passwords obejects to our passwords_list
         """
@@ -41,6 +39,15 @@ class TestCredentials(unittest.TestCase):
         test_passwords.save_passwords()
         self.assertEqual(len(User.passwords_list),2)
 
+ def test_delete_passwords(self):
+        """
+        test_delete_passwords to test if we can remove a password from our password list
+        """
+        self.new_passwords.save_passwords()
+        test_passwords = User("Cheryl","Hadassah","chadassah@gmail.com","Chada12")
+        test_passwords.save_passwords()
+        self.new_passwords.delete_password()
+        self.assertEqual(len(User.passwords_list),1)
 
 if __name__ == '__main__':
     unittest.main()
