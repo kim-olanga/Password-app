7bc1726117f564fdf60bfecf99668aa46f980c49
Kimberly <kimolanga123@gmail.com>
2022-04-24 12:00:20 +0300

    add class credentials

---
+++ password_test.py
@@ -0,0 +1,10 @@
+import unittest
+from password import User
+
+class TestCredentials(unittest.TestCase):
+    """
+    Test class that defines test cases for the User class behaviors.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    """
