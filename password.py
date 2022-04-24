5341c17c3df6c4a6e32572062115119555a09f84
Kimberly <kimolanga123@gmail.com>
2022-04-24 15:21:40 +0300

    add input the print function

--- password.py
+++ password.py
@@ -5,15 +5,6 @@ class User:
 
     passwords_list = [] #List is empty..
     def __init__(self,first_name,last_name,email,passwords):
-        """
-        __init__ methods that helps us define properties for our objects.
-
-        Args:
-            first_name:New password first name.
-            last_name:New password last name.
-            email:New password email address.
-            password:New password password.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
