dcfb58c6afceb80a9e22edabd83d1008ba82c089
Kimberly <kimolanga123@gmail.com>
2022-04-24 11:51:02 +0300

    add user claass

---
+++ password.py
@@ -0,0 +1,21 @@
+class User:
+    """
+    class that generates new instances of passwords
+    """
+
+    password_list = [] #List is empty..
+    def __init__(self,first_name,last_name,email,password):
+            # """
+            # __init__ methods that helps us define properties for our objects.
+
+            # Args:
+            #     first_name:New password first name.
+            #     last_name:New password last name.
+            #     email:New password email address.
+            #     password:New password password.
+            # """
+
+            self.first_name = first_name
+            self.last_name = last_name
+            self.email = email
+            self.password = password
